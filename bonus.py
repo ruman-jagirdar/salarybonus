923df28fe936d74f80f7bb3c1ee4329e9bb7621d
ruman-jagirdar <rumanjagirdar08@gmail.com>
2025-11-22 12:37:41 +0530

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -3,6 +3,11 @@ import sys
 if len(sys.argv) != 2:
     print("Usage: python salary.py <salary>")
     sys.exit(1)
+else:
+    salary = float(sys.argv[1])
+    bonus = salary * 0.10
+    total_salary = salary + bonus
+
 
 salary = float(sys.argv[1])
 
